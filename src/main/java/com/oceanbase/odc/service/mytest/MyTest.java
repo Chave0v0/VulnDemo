f7b9b4935caf3144ec8e1b05901cf43694adee63
Chave <chennehc1024@gmail.com>
2024-10-10 13:49:43 +0800

    Create MyTest.java

---
+++ src/main/java/com/oceanbase/odc/service/mytest/MyTest.java
@@ -0,0 +1,10 @@
+package com.oceanbase.odc.service.mytest;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Test {
+    public void vulMethod(){
+        System.out.println("test");
+    }
+}
